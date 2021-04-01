f31ee7e7f644c4510e6f98bf904ae9c209afa2b1
Milenagms <milenagomes022@gmail.com>
2021-04-01 15:26:57 -0300

    APRENDENDO VARIÁVEIS

---
+++ conhecendo_variaveis.js
@@ -0,0 +1,21 @@
+/*
+# VARIÁVEIS 
+    * Nomes simbólicos para receber algum valor 
+    * Atalhos de código 
+    * Identificadores 
+    *  3 palavras reservadas para criar uma variável 
+        * var 
+        * let
+        * const - essa variável não muda, igual as outras. Só terá um valor.
+*/
+
+// var clima ="Quente"
+// clima = "Frio"
+// console.log(clima)
+
+//let clima ="Quente"
+//console.log(clima)
+
+const clima ="Quente"
+console.log(clima)
+
